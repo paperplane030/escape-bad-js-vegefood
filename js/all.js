d67193d33b918fc0fc86cd2626733fc117a0f7f9
paperplane030 <dinosaur0023@gmail.com>
2022-03-08 16:24:44 +0800

    嘗試修正

--- js/all.js
+++ js/all.js
@@ -1,57 +1,49 @@
+/* global axios:readonly */
 // TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 QQ
-var url="https://hexschool.github.io/js-filter-data/data.json"
-var data;
-
-axios.get(url)
- .then(function(res){
-  data=res.data.filter(a=>a.作物名稱)
-  // TODO: 之後拆成 renderData 函式
-  var str=""
-  data.forEach((b,index)=>{
+const url = 'https://hexschool.github.io/js-filter-data/data.json';
+const table = document.querySelector('.table-content');
+const filter = document.querySelector('.filter');
+let data;
+let showData = [];
+let category = '';
+function renderData(_data) {
+  let str = '';
+  _data.forEach((b) => {
     // TODO: 改成 ES6 的 Template Literals (字面字串符)
-    var content="<tr><td>" + b.作物名稱 
-    +"</td><td>" + b.市場名稱
-    +"</td><td>" + b.上價
-    +"</td><td>" + b.中價
-    +"</td><td>" + b.下價
-    +"</td><td>" + b.平均價
-    +"</td><td>" + b.交易量
-    +"</td></tr>"
-    str+=content
-  })
-  table.innerHTML=str
-})
+    const content = `<tr><td>
+      ${b.作物名稱}
+      </td><td> 
+      ${b.市場名稱}
+      </td><td> 
+      ${b.上價} 
+      </td><td> 
+      ${b.中價}
+      </td><td>
+      ${b.下價}
+      </td><td>
+      ${b.平均價}
+      </td><td>
+      ${b.交易量}
+      </td></tr>`;
+    str += content;
+  });
+  return str;
+}
 
-var table=document.querySelector(".table-content")
-var showData=[]
-
-var category=""
-var filter=document.querySelector(".filter")
-
-filter.addEventListener("click",filterCategory)
-
-function filterCategory(e){
-  if(e.target.nodeName=="BUTTON"){
-    category=e.target.dataset.category
-    showData=data.filter((i)=>{
-      return i.種類代碼==category
-    })
+function filterCategory(e) {
+  if (e.target.nodeName === 'BUTTON') {
+    category = e.target.dataset.category;
+    showData = data.filter((i) => i.種類代碼 === category);
     // TODO: 之後拆成 renderData 函式
-    var str=""
-    showData.forEach((i,index)=>{
-      var content="<tr><td>" + i.作物名稱 
-      +"</td><td>" + i.市場名稱
-      +"</td><td>" + i.上價
-      +"</td><td>" + i.中價
-      +"</td><td>" + i.下價
-      +"</td><td>" + i.平均價
-      +"</td><td>" + i.交易量
-      +"</td></tr>"
-      str+=content
-    })
-    table.innerHTML=str
-  }else{
-    return;
+    const str = renderData(showData);
+    table.innerHTML = str;
   }
 }
 
+axios.get(url).then((res) => {
+  data = res.data.filter((a) => a.作物名稱);
+  // TODO: 之後拆成 renderData 函式
+  const str = renderData(data);
+  table.innerHTML = str;
+});
+filter.addEventListener('click', filterCategory);
